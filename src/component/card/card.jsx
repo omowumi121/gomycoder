10246d41ae0160868343567f1a0fa3cc5a4e95bf
omowumi121 <gabrielomowunmi2@gmail.com>
2025-02-23 12:13:08 +0100

    second comit

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -9,7 +9,7 @@ function Card (){
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">
-          Some quick example text to build on the card title and make up the bulk of the cards content.
+          Some quick example text to build on the card  and make up the bulk of the cards content.
         </p>
         <a href="#" className="btn btn-primary">
           Go somewhere
